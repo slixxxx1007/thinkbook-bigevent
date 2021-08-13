078570b8c99eeb7e654f90e08c10a391b9d0dc43
sunlixiang <x501898513@163.com>
2021-08-13 19:24:50 +0800

    完成了登录和注册功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,5 @@
 // $.ajaxPrefilter是jq的方法，在发起get，post，ajax前调用
 $.ajaxPrefilter(function(options) {
     options.url = "http://www.liulongbin.top:3008" + options.url
+        // console.log(options);
 })
